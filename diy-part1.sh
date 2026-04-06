804d7f94d93d28a071b58e47f49c2318ba0a75f9
ohayobot <95493890+ohayobot@users.noreply.github.com>
2026-04-07 00:36:58 +0800

    更新 diy-part1.sh
    清空

--- diy-part1.sh
+++
@@ -1,5 +0,0 @@
-# 移除可能冲突的源
-sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-
-# 添加核心插件源 (只保留这一个最全的，减少冲突)
-sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
