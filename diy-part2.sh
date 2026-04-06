a9df9ec7f9a5887b1100b65c97915a60bc6ec79f
ohayobot <95493890+ohayobot@users.noreply.github.com>
2026-04-07 00:38:42 +0800

    更新 diy-part2.sh
    解决源错误

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,5 +1,10 @@
-# 修改默认 IP 为 192.168.10.10
+# 1. 修改默认 IP 为 192.168.10.10
 sed -i 's/192.168.1.1/192.168.10.10/g' package/base-files/files/bin/config_generate
 
-# 修改默认主机名为 ImmortalWrt
-sed -i 's/OpenWrt/ImmortalWrt/g' package/base-files/files/bin/config_generate
+# 2. 强行把插件包直接克隆进 package 文件夹 (跳过 Feeds 系统)
+# 我们只拉取最核心的两个仓库，深度设为 1 速度最快
+git clone --depth 1 https://github.com/kenzok8/openwrt-packages package/openwrt-packages
+git clone --depth 1 https://github.com/kenzok8/small-package package/small-package
+
+# 3. (可选) 如果你以后想换主题，可以把下面这行也加上
+git clone --depth 1 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
